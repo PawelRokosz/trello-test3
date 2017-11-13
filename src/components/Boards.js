b87b7d033fb35284e999450098b829d0c180173f
Pawel <rokosz.pawel@gmail.com>
2017-11-13 14:38:07 +0100

    splice implememented

--- src/components/Boards.js
+++ src/components/Boards.js
@@ -29,7 +29,6 @@ class Boards extends Component {
   }
 
   handleDragLeave() {
-    // console.log('dragLeave');
     this.setState({
       endBoard: null
     })
@@ -39,66 +38,27 @@ class Boards extends Component {
     e.preventDefault();
   }
 
-  handleDragEnd() {
-    // console.log('dragEnd startBoard', this.state.startBoard);
-    // console.log('dragEnd endBoard', this.state.endBoard);
-    // console.log('DragEnd');
-
-  }
-
   handleDrop() {
-    // console.log('drop')
     let startBoard = this.state.startBoard;
     let endBoard = this.state.endBoard;
     let data = this.state.data;
-    // console.log('startBoard', startBoard);
-    // console.log('endBoard', endBoard);
-
-    // let dataBoardStart = data.find(board => {
-    //   return board.board === startBoard;
-    // })
-    // let dataBoardEnd = data.find(board => {
-    //   return board.board === endBoard;
-    // })
-    // console.log('dataBoardStart', dataBoardStart);
-    // console.log('dataBoardEnd', dataBoardEnd);
 
     let dataBoardStart = data[startBoard];
     let dataBoardEnd = data[endBoard];
-    // console.log('start:', dataBoardStart, 'end:', dataBoardEnd);
 
     let newData = data.slice();
-    // let newDataBoardStart = newData[startBoard];
-    // let newDataBoardEnd = newData[endBoard];
-
-    // console.log('newDataBoardStart', newDataBoardStart);
-    // console.log('newDataBoardEnd', newDataBoardEnd);
 
     if (startBoard === endBoard || endBoard === undefined || !endBoard) {
       return;
     } else {
-      // console.log('newData1', newData[data.indexOf(dataBoardStart)]);
-      // console.log('dataBoardStart', dataBoardStart);
-      // console.log('newData2', newData[data.indexOf(dataBoardEnd)]);
-      // console.log('dataBoardEnd', dataBoardEnd);
-
-      // newData.splice(newData.indexOf(newDataBoardStart), 1);
-
-      newData[data.indexOf(dataBoardStart)] = dataBoardEnd;
-      newData[data.indexOf(dataBoardEnd)] = dataBoardStart;
-      //
-      // console.log('oldData', this.state.data);
-      // console.log('newData', newData);
+      newData.splice(data.indexOf(dataBoardStart), 1, dataBoardEnd);
+      newData.splice(data.indexOf(dataBoardEnd), 1, dataBoardStart);
 
       this.setState({
         data: newData
-      }, () => {
-        console.log('oldData post update', this.state.data);
       })
     }
-
   }
-
 
   render() {
     return (
@@ -110,7 +70,6 @@ class Boards extends Component {
                   onDragLeave={() => this.handleDragLeave()}
                   onDragOver={(e) => this.handleDragOver(e)}
                   onDrop={() => this.handleDrop()}
-                  onDragEnd={() => this.handleDragEnd()}
                   >
                   {board.board}
                  </div>
